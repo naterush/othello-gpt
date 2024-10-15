85238bd78d41a15e32aaad922cb4a403fd11102a
Nate Rush <naterush1997@gmail.com>
2024-10-15 21:55:53 +0000

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 # Prompt for Githuba access token interactive
-echo "Please provide a Github access token with the following permissions:"
-echo "repo, read:org, workflow"
-read -p "Github Access Token: " token
+read -p "Github Access Token: " github_token
+
+# Login with GCloud
+gcloud init
+gcloud auth application-default login
 
 # Install Github CLI
 sudo snap install gh
 
 # Set Github access token
 echo "Setting Github access token..."
-gh auth login --with-token <<< $token
+gh auth login --with-token <<< $github_token
+
+git config --global user.email "naterush1997@gmail.com"
+git config --global user.name "Nate Rush"
+
+
+# Install Google Cloud Storage Connectors
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
+sudo apt-get update
+sudo apt-get install gcsfuse
+
+# Actually attach the bucket
+gcsfuse othello-gpt ./bucket
